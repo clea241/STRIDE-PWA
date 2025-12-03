dd605cd253c74546ed1b9b6b89c2000f4a4ec0bd
kleincatapang <klein.catapang@deped.gov.ph>
2025-12-03 16:06:42 +0800

    insighted files

---
+++ www/my_dashboard/InsightEd/src/modules/Activity.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Activity = () => (
+    <div style={{ padding: '20px', paddingBottom: '80px' }}>
+        <h1>Recent Activities</h1>
+        <p>List of recent updates...</p>
+    </div>
+);
+
+export default Activity;
